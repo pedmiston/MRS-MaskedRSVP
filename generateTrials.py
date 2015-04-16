985c32efe9dbfa23e225a240ff368e5df772f80e
pedmiston <pierce.edmiston@gmail.com>
2015-04-16 13:23:09 -0500

    Updated generate trials for MRS exp

--- generateTrials.py
+++ generateTrials.py
@@ -1,97 +1,165 @@
 #!/usr/bin/env python
-
+from collections import OrderedDict
+from itertools import product
 import random
 import pandas as pd
 import numpy as np
 
-def circularList(lst,seed):
-    if not isinstance(lst,list):
-        lst = range(lst)
-    i = 0
+def chooseTargetAndFoil(name, opts):
+    """
+    opts is a DataFrame with columns 'picName' and 'picFile'
+    """
+    choices = opts[opts.picName == name]
+    pics = choices.picFile.tolist()
+    assert len(pics) == 2, \
+        "There were more than 2 pics with the name {}".format(name)
+    random.shuffle(pics)
+    return pics
+
+def listToLine(orderedList):
+    stringParams = map(str, orderedList)
+    trialRow = ','.join(stringParams) + '\n'
+    return trialRow
+
+def main(trialsFileName, seed):
     random.seed(seed)
-    random.shuffle(lst)
-    while True:
-        yield lst[i]
-        if (i+1) % len(lst) ==0:
-            random.shuffle(lst)
-        i = (i + 1)%len(lst)
 
-pics = pd.read_csv('targets.csv', names = ['category', ])['category'].tolist()
-foils = pd.read_csv('foils.csv', names = ['category', ])['category'].tolist()
+    orderedFields = (
+        'part', 'blockNum',
+        'targetName', 'targetFile', 'foilFile', 'whenTargetName',
+        'isMask', 'whenMask',
+        'picDurationType', 'picDurationSec', 'targetPos',
+        'picFile1', 'picFile2', 'picFile3', 'picFile4', 'picFile5', 'picFile6',
+        'isTargetPresent', 'whichTarget',
+    )
 
-criticalTrialType = ['foil']*2 + ['category'] + ['match']*2
-criticalTrialType *= 3
+    curTrial = OrderedDict()
+    for field in orderedFields:
+        curTrial[field] = ''
 
-#distribution = ['.40', '.20', '.40'] #don't know how to integrate this in to ensure the right distribution..want 7 foils, 4 category, 7 match trials
-cues = list(pics)
-isMatch = ['0','0','1']
-separator = ","
-numIter = 1
-picNum = range(1, 7)
+    whenTargetNameOpts = ['before', 'after']
+    picDurationTypeOpts = ['long', 'short']
+    picDurationMap = {'long': 0.083, 'short': 0.025}
 
-##there are 18 targets. each is presented at the beginning of a trial (so 18 trials) and this is
-##the picture the participant needs to look for. After the target, the participant sees another
-## 6 pictures. On picture 2-5 of these 6 is the critical trial. On 7 of these trials, the critical
-##picture is the target picture. On another 7, it is a random foil. On the remaining 4 trials, the critical
-##picture is a category match to the target. These have the same names except targets' names end with
-## 1 and category match names end with 2.
+    blockOpts = {}
+    blockOpts['whenTargetName'] = whenTargetNameOpts
+    blockOpts['picDurationType'] = picDurationTypeOpts
+    blockItems = list(product(*blockOpts.values()))
+    blocks = pd.DataFrame(blockItems, columns = blockOpts.keys())
 
-#In the before group, each trial began with a
-#fixation cross for 500 ms, followed by the name of the target
-#for 700 ms, a 200-ms blank screen, and then the sequence of
-#pictures. (from a recent RSVP paper)
+    blocks['part'] = 'test'
 
-##picture durations: target stays up for X; trial pics stay up for X. ISI is
+    practiceBlockInfo = {}
+    practiceBlockInfo['whenTargetName'] = 'before'
+    practiceBlockInfo['picDurationType'] = 'long'
+    practiceBlockInfo['part'] = 'practice'
+    practiceBlock = pd.DataFrame(practiceBlockInfo, index = [-1, ])
 
-def randomButNot (arr, index):
-    randIndex=index
-    while randIndex == index:
-        randIndex = random.randint(0, len(arr)-1)
-    return arr[randIndex]
+    blocks = blocks.append(practiceBlock).sort()
 
-def main(subjCode,seed):
-    testFile = open('trialList_test_'+subjCode+ '.csv','w')
-    print >>testFile, separator.join(("block", "picCategory", "targetPic", "criticalTrialType", "criticalPicFile", "criticalTrialNum", "picFile1", "picFile2", "picFile3", "picFile4", "picFile5", "picFile6", "isMatch"))
-    random.seed(int(seed))
-    np.random.seed(int(seed)) #if you're using the numpy random number generator, you need to set the seed for it separately (!)
-    trialList = []
-    trialTypeToPic = {'match':'1', 'category':'2'}
-    trialTypeToMatch = {'match':'1', 'category':'0'}
-    conditionList = circularList(criticalTrialType,seed)
-    numBlocks = 5
+    numPracticeTrials = 4
+    trialsPerBlock = 80
+    proportionTargetPresent = 0.75
 
-    for curBlock in range(numBlocks):
-        trialList.append([])
-        for picIndex,curPic in enumerate(pics):
-            curTarget = curPic + "1"
+    targetPresentTrials = int(trialsPerBlock * proportionTargetPresent)
+    targetAbsentTrials = trialsPerBlock - targetPresentTrials
 
-            curTrialType = conditionList.next()
-            curCriticalTrialNum = np.random.choice([2,3,4,5], 1)[0]
-            distractorPics = np.random.choice(foils, 6, replace = False).tolist()
+    withinBlockOpts = {}
+    withinBlockOpts['isTargetPresent'] = [1,] * targetPresentTrials + \
+                                         [0,] * targetAbsentTrials
+    assert targetPresentTrials % 2 == 0, \
+        ("If there aren't an even number of target present trials,"
+         "the mask can't be evenly split")
+    withinBlockOpts['isMask'] = [1, 0] * (trialsPerBlock/2)
 
-            curTrialPics = {}
-            for curPicNum in picNum:
-                picCol = "pic"+str(curPicNum)+"File"
-                if curPicNum == curCriticalTrialNum:
-                    if curTrialType == "foil":
-                        curPicFile = distractorPics.pop()
-                        curIsMatch = 0
-                    else:
-                        curPicFile = curPic + trialTypeToPic[curTrialType]
-                        curIsMatch = trialTypeToMatch[curTrialType]
-                
+    withinBlockTrials = pd.DataFrame(withinBlockOpts)
 
-                    # duplicate information in the pic column
-                    curTrialPics[curPicNum] = curPicFile
+    targets = pd.read_csv('stimuli/targets.csv')
+    targetNameList = targets.picName.unique().tolist()
+    random.shuffle(targetNameList)
+
+    distractors = pd.read_csv('stimuli/distractors.csv', names = ['picFile', ])
+    distractorList = distractors.picFile.tolist()
+    random.shuffle(distractorList)
+
+    trialList = open(trialsFileName + '.csv', 'w')
+    header = listToLine(curTrial.keys())
+    trialList.write(header)
+
+    for blockNum, blockInfo in blocks.iterrows():
+        curBlock = []
+
+        curPart = blockInfo['part']
+        whenTargetName = blockInfo['whenTargetName']
+        picDurationType = blockInfo['picDurationType']
+        picDurationSec = picDurationMap[picDurationType]
+
+        curTrial['part'] = curPart
+        curTrial['blockNum'] = blockNum
+        curTrial['whenTargetName'] = whenTargetName
+        curTrial['picDurationType'] = picDurationType
+        curTrial['picDurationSec'] = picDurationSec
+
+        for trialN, trialInfo in withinBlockTrials.iterrows():
+            if curPart == 'practice' and trialN > numPracticeTrials:
+                break
+
+            curTrial['isMask'] = trialInfo['isMask']
+            curTrial['isTargetPresent'] = trialInfo['isTargetPresent']
+            curTrial['whenMask'] = curTrial['whenTargetName']
+
+            # determine the target name to appear as text in this trial
+            try:
+                targetName = targetNameList.pop()
+            except IndexError:
+                print 'Ran out of unique targets! Resetting...'
+                targetNameList = targets.picName.unique().tolist()
+                random.shuffle(targetNameList)
+                targetName = targetNameList.pop()
+            curTrial['targetName'] = targetName
+
+            # set the parameters for target present|absent trials
+            if curTrial['isTargetPresent'] == 1:
+                targetFile, foilFile = chooseTargetAndFoil(targetName, targets)
+                targetPos = random.choice([2, 3, 4, 5])
+                whichTarget = random.choice(['left', 'right'])
+
+                curTrial['targetFile'] = targetFile
+                curTrial['foilFile'] = foilFile
+                curTrial['targetPos'] = targetPos
+                curTrial['whichTarget'] = whichTarget
+            else:
+                stringForNAValue = 'NA'
+                curTrial['targetFile'] = stringForNAValue
+                curTrial['foilFile'] = stringForNAValue
+                curTrial['targetPos'] = -1
+                curTrial['whichTarget'] = stringForNAValue
+
+            # determine pictures in the sequence
+            for picPos in [1, 2, 3, 4, 5, 6]:
+                picFileID = 'picFile{}'.format(picPos)
+
+                if picPos == curTrial['targetPos']:
+                    picFile = curTrial['targetFile']
                 else:
-                    curTrialPics[curPicNum] = distractorPics.pop() + ""
+                    try:
+                        picFile = distractorList.pop()
+                    except IndexError:
+                        print 'Ran out of unique distractors! Resetting...'
+                        distractorList = distractors.picFile.tolist()
+                        random.shuffle(distractorList)
+                        picFile = distractorList.pop()
 
-            trialList[curBlock].append(separator.join((str(curBlock+1), curPic, curTarget, curTrialType, curPicFile, str(curCriticalTrialNum), curTrialPics[1], curTrialPics[2], curTrialPics[3], curTrialPics[4], curTrialPics[5], curTrialPics[6], str(curIsMatch))))
+                curTrial[picFileID] = picFile
 
-    for curBlock in trialList:    
+            # all params for curTrial have been set. Add to curBlock
+            curBlock.append(curTrial.values())
+
+        # all trials for curBlock have been created. Shuffle and write
         random.shuffle(curBlock)
-        for curTrialList in curBlock:    
-            print >>testFile, curTrialList
+        for trialParams in curBlock:
+            trial = listToLine(trialParams)
+            trialList.write(trial)
 
 if __name__ == "__main__":
-    trialList = main('testTrials-13',13)
+    main('test-trials', 13)
